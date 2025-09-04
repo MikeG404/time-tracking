13dd0412a6f366ef801ad822504f851d46beca57
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-04 09:06:19 +0200

    Enhance text Card ID

--- src/components/CardID.jsx
+++ src/components/CardID.jsx
@@ -4,7 +4,7 @@ import avatar from '/public/images/image-jeremy.png'
 export default function CardID({ color }) {
   return (
     <div className='flex flex-col gap-6 bg-blue-950 rounded-2xl md:col-span-3 xl:col-span-1 xl:row-span-2'>
-        <div className='id-content flex flex-row items-center gap-6 rounded-2xl p-8' style={{ backgroundColor: color }}>
+        <div className='id-content flex flex-row items-center gap-6 rounded-2xl p-8 xl:flex-col xl:items-start xl:gap-10 xl:p-8' style={{ backgroundColor: color }}>
             <div className='w-16 h-16'>
                 <img src={avatar} alt='avatar' className='border-2 border-white rounded-full' />
             </div>
@@ -13,7 +13,7 @@ export default function CardID({ color }) {
                 <h2 className='text-white text-preset-4'>Jeremy Robson</h2>
             </div>
         </div>
-        <nav className='flex flex-row justify-evenly pb-6 gap-4'>
+        <nav className='flex flex-row justify-evenly pb-6 gap-4 xl:flex-col xl:items-start xl:pl-8'>
             <button className='text-white text-preset-5-regular'>Daily</button>
             <button className='text-white text-preset-5-regular'>Weekly</button>
             <button className='text-white text-preset-5-regular'>Monthly</button>
