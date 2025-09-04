888477df7423027ff09c5738e12c3aa601e4da87
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-04 09:25:55 +0200

    Add state to change data daily, weekly, monthly

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,208 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './components/Card'
 import CardID from './components/CardID'
 
-const data = [
-  {
-    title: 'Work',
-    image: '/images/icon-work.svg',
-    color: 'var(--orange-300)',
-    timeframe: {
-      current: 32,
-      previous: 36,
+const data = {
+  daily: [
+    {
+      title: 'Work',
+      image: '/images/icon-work.svg',
+      color: 'var(--orange-300)',
+      timeframe: {
+        current: 5,
+        previous: 7,
+      },
     },
-  },
-  {
-    title: 'Play',
-    image: '/images/icon-play.svg',
-    color: 'var(--blue-300)',
-    timeframe: {
-      current: 10,
-      previous: 8,
+    {
+      title: 'Play',
+      image: '/images/icon-play.svg',
+      color: 'var(--blue-300)',
+      timeframe: {
+        current: 1,
+        previous: 2,
+      },
     },
-  },
-  {
-    title: 'Study',
-    image: '/images/icon-study.svg',
-    color: 'var(--red-400)',
-    timeframe: {
-      current: 4,
-      previous: 7,
+    {
+      title: 'Study',
+      image: '/images/icon-study.svg',
+      color: 'var(--red-400)',
+      timeframe: {
+        current: 0,
+        previous: 1,
+      },
     },
-  },
-  {
-    title: 'Exercise',
-    image: '/images/icon-exercise.svg',
-    color: 'var(--green-400)',
-    timeframe: {
-      current: 4,
-      previous: 5,
+    {
+      title: 'Exercise',
+      image: '/images/icon-exercise.svg',
+      color: 'var(--green-400)',
+      timeframe: {
+        current: 1,
+        previous: 1,
+      },
     },
-  },
-  {
-    title: 'Social',
-    image: '/images/icon-social.svg',
-    color: 'var(--purple-700)',
-    timeframe: {
-      current: 5,
-      previous: 10,
+    {
+      title: 'Social',
+      image: '/images/icon-social.svg',
+      color: 'var(--purple-700)',
+      timeframe: {
+        current: 1,
+        previous: 3,
+      },
     },
-  },
-  {
-    title: 'Self Care',
-    image: '/images/icon-self-care.svg',
-    color: 'var(--yellow-300)',
-    timeframe: {
-      current: 2,
-      previous: 2,
+    {
+      title: 'Self Care',
+      image: '/images/icon-self-care.svg',
+      color: 'var(--yellow-300)',
+      timeframe: {
+        current: 0,
+        previous: 1,
+      },
     },
-  },
-]
+  ],
+  weekly: [
+    {
+      title: 'Work',
+      image: '/images/icon-work.svg',
+      color: 'var(--orange-300)',
+      timeframe: {
+        current: 32,
+        previous: 36,
+      },
+    },
+    {
+      title: 'Play',
+      image: '/images/icon-play.svg',
+      color: 'var(--blue-300)',
+      timeframe: {
+        current: 10,
+        previous: 8,
+      },
+    },
+    {
+      title: 'Study',
+      image: '/images/icon-study.svg',
+      color: 'var(--red-400)',
+      timeframe: {
+        current: 4,
+        previous: 7,
+      },
+    },
+    {
+      title: 'Exercise',
+      image: '/images/icon-exercise.svg',
+      color: 'var(--green-400)',
+      timeframe: {
+        current: 4,
+        previous: 5,
+      },
+    },
+    {
+      title: 'Social',
+      image: '/images/icon-social.svg',
+      color: 'var(--purple-700)',
+      timeframe: {
+        current: 5,
+        previous: 10,
+      },
+    },
+    {
+      title: 'Self Care',
+      image: '/images/icon-self-care.svg',
+      color: 'var(--yellow-300)',
+      timeframe: {
+        current: 2,
+        previous: 2,
+      },
+    },
+  ],
+  monthly: [
+    {
+      title: 'Work',
+      image: '/images/icon-work.svg',
+      color: 'var(--orange-300)',
+      timeframe: {
+        current: 103,
+        previous: 128,
+      },
+    },
+    {
+      title: 'Play',
+      image: '/images/icon-play.svg',
+      color: 'var(--blue-300)',
+      timeframe: {
+        current: 23,
+        previous: 29,
+      },
+    },
+    {
+      title: 'Study',
+      image: '/images/icon-study.svg',
+      color: 'var(--red-400)',
+      timeframe: {
+        current: 13,
+        previous: 19,
+      },
+    },
+    {
+      title: 'Exercise',
+      image: '/images/icon-exercise.svg',
+      color: 'var(--green-400)',
+      timeframe: {
+        current: 11,
+        previous: 18,
+      },
+    },
+    {
+      title: 'Social',
+      image: '/images/icon-social.svg',
+      color: 'var(--purple-700)',
+      timeframe: {
+        current: 21,
+        previous: 23,
+      },
+    },
+    {
+      title: 'Self Care',
+      image: '/images/icon-self-care.svg',
+      color: 'var(--yellow-300)',
+      timeframe: {
+        current: 7,
+        previous: 11,
+      },
+    },
+  ],
+}
 
 function App() {
+  const [selectedTimeframe, setSelectedTimeframe] = useState('weekly')
+
   return (
-    <div className='min-h-screen max-w-80 my-[81px] flex justify-center items-center w-full md:max-w-[622px] xl:max-w-[829px]'>
+    <div className='min-h-screen p-4 flex justify-center items-center'>
       <div 
         className='
           grid 
           grid-cols-1 
           md:grid-cols-3
-          md:grid-rows-3
           xl:grid-cols-4
-          xl:grid-rows-2
-          gap-4 w-full'
+          gap-6 
+          w-full 
+          max-w-sm
+          md:max-w-4xl
+          xl:max-w-6xl'
         >
-        <CardID color='var(--purple-600)' />
-        {data.map((item, index) => (
+        <CardID 
+          color='var(--purple-600)' 
+          selectedTimeframe={selectedTimeframe}
+          onTimeframeChange={setSelectedTimeframe}
+        />
+        {data[selectedTimeframe].map((item, index) => (
           <Card 
             key={index}
             title={item.title} 
             image={item.image} 
             color={item.color} 
-            timeframe={item.timeframe} 
+            timeframe={item.timeframe}
+            selectedTimeframe={selectedTimeframe}
           />
         ))}
       </div>
--- src/components/CardID.jsx
+++ src/components/CardID.jsx
@@ -1,22 +1,37 @@
 import React from 'react'
 import avatar from '/public/images/image-jeremy.png'
 
-export default function CardID({ color }) {
+export default function CardID({ color, selectedTimeframe, onTimeframeChange }) {
+  const timeframes = ['daily', 'weekly', 'monthly']
+  
   return (
-    <div className='flex flex-col gap-6 bg-blue-950 rounded-2xl md:col-span-3 xl:col-span-1 xl:row-span-2'>
-        <div className='id-content flex flex-row items-center gap-6 rounded-2xl p-8 xl:flex-col xl:items-start xl:gap-10 xl:p-8' style={{ backgroundColor: color }}>
+    <div className='flex flex-col bg-blue-950 rounded-2xl md:col-span-3 xl:col-span-1 xl:row-span-2'>
+        <div className='flex flex-row items-center gap-6 rounded-2xl p-8 xl:flex-col xl:items-start xl:gap-10 xl:p-8' style={{ backgroundColor: color }}>
             <div className='w-16 h-16'>
                 <img src={avatar} alt='avatar' className='border-2 border-white rounded-full' />
             </div>
             <div className='id-content-text'>
                 <h1 className='text-white text-preset-6'>Report for</h1>
-                <h2 className='text-white text-preset-4'>Jeremy Robson</h2>
+                <h2 className='text-white text-preset-4'>
+                    Jeremy 
+                    Robson
+                </h2>
             </div>
         </div>
-        <nav className='flex flex-row justify-evenly pb-6 gap-4 xl:flex-col xl:items-start xl:pl-8'>
-            <button className='text-white text-preset-5-regular'>Daily</button>
-            <button className='text-white text-preset-5-regular'>Weekly</button>
-            <button className='text-white text-preset-5-regular'>Monthly</button>
+        <nav className='flex flex-row justify-evenly pb-6 gap-4 xl:flex-col xl:items-start xl:pl-8 xl:flex-1'>
+            {timeframes.map((timeframe) => (
+                <button 
+                    key={timeframe}
+                    onClick={() => onTimeframeChange(timeframe)}
+                    className={`text-preset-5-regular capitalize ${
+                        selectedTimeframe === timeframe 
+                            ? 'text-white' 
+                            : 'text-white/60 hover:text-white'
+                    }`}
+                >
+                    {timeframe}
+                </button>
+            ))}
         </nav>
     </div>
   )
