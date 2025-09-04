83b94ee28bdff28c2af8a7e811162f1548dfee32
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-04 09:36:14 +0200

    Add shadow effects to avatar and improve CardID styling

--- src/components/CardID.jsx
+++ src/components/CardID.jsx
@@ -7,12 +7,12 @@ export default function CardID({ color, selectedTimeframe, onTimeframeChange }) 
   return (
     <div className='flex flex-col bg-blue-950 rounded-2xl md:col-span-3 xl:col-span-1 xl:row-span-2'>
         <div className='flex flex-row items-center gap-6 rounded-2xl p-8 xl:flex-col xl:items-start xl:gap-10 xl:p-8' style={{ backgroundColor: color }}>
-            <div className='w-16 h-16'>
-                <img src={avatar} alt='avatar' className='border-2 border-white rounded-full' />
+            <div className='w-16 h-16 shadow-2xl'>
+                <img src={avatar} alt='avatar' className='border-2 border-white rounded-full shadow-2xl' />
             </div>
             <div className='id-content-text'>
                 <h1 className='text-white text-preset-6'>Report for</h1>
-                <h2 className='text-white text-preset-4'>
+                <h2 className='text-white text-preset-4 '>
                     Jeremy 
                     Robson
                 </h2>
