903ef8655340754947e0a1f2cbe231b4bc9a0f45
Mike Godfrinne <mike.godfrinne@gmail.com>
2025-09-04 06:14:55 +0200

    responsive card made

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,70 @@
 import React from 'react'
 import Card from './components/Card'
 
+const data = [
+  {
+    title: 'Work',
+    image: '/images/icon-work.svg',
+    color: 'var(--orange-300)',
+    timeframe: {
+      current: 32,
+      previous: 36,
+    },
+  },
+  {
+    title: 'Play',
+    image: '/images/icon-play.svg',
+    color: 'var(--blue-300)',
+    timeframe: {
+      current: 10,
+      previous: 8,
+    },
+  },
+  {
+    title: 'Study',
+    image: '/images/icon-study.svg',
+    color: 'var(--red-400)',
+    timeframe: {
+      current: 4,
+      previous: 7,
+    },
+  },
+  {
+    title: 'Exercise',
+    image: '/images/icon-exercise.svg',
+    color: 'var(--green-400)',
+    timeframe: {
+      current: 4,
+      previous: 5,
+    },
+  },
+  {
+    title: 'Social',
+    image: '/images/icon-social.svg',
+    color: 'var(--purple-700)',
+    timeframe: {
+      current: 5,
+      previous: 10,
+    },
+  },
+  {
+    title: 'Self Care',
+    image: '/images/icon-self-care.svg',
+    color: 'var(--yellow-300)',
+    timeframe: {
+      current: 2,
+      previous: 2,
+    },
+  },
+]
+
 function App() {
   return (
-    <div className='h-screen flex items-center justify-center bg-navy-950'>
-      <div className='w-full max-w-7xl grid grid-cols-1 md:grid-cols-3 gap-4'>
-        <Card timeframe="daily" title="Work" />
-        <Card timeframe="weekly" title="Play" />
-        <Card timeframe="monthly" title="Study" />
-        <Card timeframe="daily" title="Exercise" />
-        <Card timeframe="weekly" title="Social" />
-        <Card timeframe="monthly" title="Self Care" />
+    <div className='h-screen flex items-center justify-center'>
+      <div className='grid-container'>
+        {data.map((item) => (
+          <Card timeframe="daily" title={item.title} image={item.image} color={item.color} timeframe={item.timeframe} />
+        ))}
       </div>
     </div>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import ellipses from '/public/images/icon-ellipsis.svg'
 
-export default function Card({ image, title, timeframe }) {
+export default function Card({ image, title, timeframe, color }) {
   return (
-        <div className='card-header relative bg-amber-300 aspect-square w-64 rounded-2xl'>
-            <div className='absolute top-0 left-0'>
+        <div className="card-header relative grid-card rounded-2xl" style={{ backgroundColor: color }}>
+            <div className='absolute top-0 right-0'>
                 <img src={image} alt={title} />
             </div>
-            <div className='card-content flex flex-col items-start gap-6 p-8 bg-blue-950 rounded-2xl max-w-[255px] w-full h-full max-h-[199px] absolute bottom-0'>
-                <h3 className='text-white text-preset-5-medium'>{title}</h3>
+            <div className='card-content flex flex-col items-start gap-6 p-8 bg-blue-950 rounded-2xl max-w-[255px] w-full absolute bottom-0'>
+                <div className='flex justify-between items-center w-full'>
+                  <h3 className='text-white text-preset-5-medium'>{title}</h3>
+                  <img src={ellipses} alt={title} />
+                </div>
                 <div className='flex flex-col gap-2'>
-                  <p className='text-white text-preset-1'>36hrs</p>
-                  <p className='text-white text-preset-6'>Last Week - 36hrs</p>
+                  <p className='text-white text-preset-1'>{timeframe.current}hrs</p>
+                  <p className='text-white text-preset-6'>Last Week - {timeframe.previous}hrs</p>
                 </div>
             </div>
     </div>
